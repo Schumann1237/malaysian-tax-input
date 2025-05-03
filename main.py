660d76266f081bef84bb20e734cb5b537d1979e0
Schumann1237 <schubertschumann2@gmail.com>
2025-05-03 19:39:04 +0800

    Add user IC registration and login flow with CSV storage

--- main.py
+++ main.py
@@ -1,4 +1,40 @@
-from functions import save_to_csv
+import pandas as pd
+from functions import verify_user
 
-user_details = input("Enter name and IC number (comma-separated): ") # Isaac,031004101725
-save_to_csv(user_details, "User-Details.csv")
+def main():
+    try:
+        df = pd.read_csv("User-Details.csv")
+    except FileNotFoundError:
+        df = pd.DataFrame({"IC Number": [], "Income": [], "Tax Relief": [], "Tax Payable": []})
+        df.to_csv("User-Details.csv", index=False)
+
+    registryCheck = input("Are you a registered user? (Y/N): ")
+
+    if registryCheck.upper() == "N":
+        while True:
+            ic_number = input("Enter your IC Number to register: ")
+            if verify_user(ic_number):
+                break
+            else:
+                print("Invalid IC Number. Format accordingly \"012345678910\" or \"012345-67-8910\" \n")
+
+        ic_df = pd.DataFrame({
+            "IC Number": [ic_number],
+            "Income": [""],
+            "Tax Relief": [""],
+            "Tax Payable": [""]
+        })
+
+        df = pd.concat([df, ic_df], ignore_index=True)
+        df.to_csv("User-Details.csv", index=False)
+    
+    elif registryCheck.upper() == "Y":
+         while True:
+            ic_number = input("Enter your IC Number: ")
+            if verify_user(ic_number):
+                break
+            else:
+                print("Invalid IC Number. Format accordingly \"012345678910\" or \"012345-67-8910\" \n")
+
+
+main()
