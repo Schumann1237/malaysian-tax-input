f116599cc2e54df564caa24714feec80ecc5ee71
Schumann1237 <schubertschumann2@gmail.com>
2025-05-04 09:52:14 +0800

    update login logic in main.py

--- main.py
+++ main.py
@@ -5,21 +5,23 @@ def main():
     try:
         df = pd.read_csv("User-Details.csv")
     except FileNotFoundError:
-        df = pd.DataFrame({"IC Number": [], "Income": [], "Tax Relief": [], "Tax Payable": []})
+        df = pd.DataFrame({"IC Number": [], "Password": [],"Income": [], "Tax Relief": [], "Tax Payable": []})
         df.to_csv("User-Details.csv", index=False)
 
     registryCheck = input("Are you a registered user? (Y/N): ")
 
     if registryCheck.upper() == "N":
         while True:
-            ic_number = input("Enter your IC Number to register: ")
-            if verify_user(ic_number):
+            print("\nRegistration\n")
+            user_ic = input("Enter IC Number to register: ")
+            user_pass = input("Enter password to register (Last 4 digits of IC Number): ")
+            if verify_user(user_ic, user_pass):
                 break
             else:
                 print("Invalid IC Number. Format accordingly \"012345678910\" or \"012345-67-8910\" \n")
 
         ic_df = pd.DataFrame({
-            "IC Number": [ic_number],
+            "IC Number": [user_ic],
             "Income": [""],
             "Tax Relief": [""],
             "Tax Payable": [""]
@@ -29,12 +31,24 @@ def main():
         df.to_csv("User-Details.csv", index=False)
     
     elif registryCheck.upper() == "Y":
-         while True:
-            ic_number = input("Enter your IC Number: ")
-            if verify_user(ic_number):
-                break
+        while True:
+            print("\nLogin\n")
+            user_ic = input("Enter IC Number: ")
+            user_pass = input("Enter password: ")
+            if verify_user(user_ic, user_pass):
+                
+                user_search = df[(df["IC Number"] == user_ic) & (df["Password"] == user_pass)]
+                if user_search.empty:
+                    print("User record not found. Please register.\n")
+                    print("Exiting program\n")
+                    return
+                else:
+                    print("User record found.")
+                    break
             else:
                 print("Invalid IC Number. Format accordingly \"012345678910\" or \"012345-67-8910\" \n")
-
+            
+    else:
+        print("Invalid input. Exiting program.")
 
 main()
