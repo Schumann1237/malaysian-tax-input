3e7ef0fc9e9e471c933a55e976a1623c9ce4f191
Schumann1237 <schubertschumann2@gmail.com>
2025-05-04 13:55:37 +0800

    refactor logic

--- functions.py
+++ functions.py
@@ -6,3 +6,54 @@ def verify_user(ic_number, password):
         return True
     else:
         return False
+
+def initialize_csv():
+    try:
+        return pd.read_csv("User-Details.csv")
+    except FileNotFoundError:
+        df = pd.DataFrame({
+            "IC Number": [],
+            "Password": [],
+            "Income": [],
+            "Tax Relief": [],
+            "Tax Payable": []
+        })
+        df.to_csv("User-Details.csv", index=False)
+        return df
+
+def register_user(df):
+    print("\nRegistration\n")
+    while True:
+        user_ic = input("Enter IC Number to register: ")
+        user_pass = input("Enter password to register: ")
+        if verify_user(user_ic):
+            new_user = pd.DataFrame({
+                "IC Number": [user_ic],
+                "Password": [user_pass],
+                "Income": [""],
+                "Tax Relief": [""],
+                "Tax Payable": [""]
+            })
+            df = pd.concat([df, new_user], ignore_index=True)
+            df.to_csv("User-Details.csv", index=False)
+            print("Registration successful.\n")
+            break
+        else:
+            print("Invalid IC Number. Format accordingly \"012345678910\" or \"012345-67-8910\"\n")
+
+def login_user(df):
+    print("\nLogin\n")
+    while True:
+        user_ic = input("Enter IC Number: ")
+        user_pass = input("Enter password: ")
+        if verify_user(user_ic):
+            user_search = df[(df["IC Number"] == user_ic) & (df["Password"] == user_pass)]
+            if user_search.empty:
+                print("User record not found. Please register.\nExiting program.\n")
+                return False
+            else:
+                print("Login successful.\n")
+                return True
+        else:
+            print("Invalid IC Number. Format accordingly \"012345678910\" or \"012345-67-8910\"\n")
+
--- main.py
+++ main.py
@@ -1,53 +1,16 @@
 import pandas as pd
-from functions import verify_user
+from functions import * 
 
 def main():
-    try:
-        df = pd.read_csv("User-Details.csv")
-    except FileNotFoundError:
-        df = pd.DataFrame({"IC Number": [], "Password": [],"Income": [], "Tax Relief": [], "Tax Payable": []})
-        df.to_csv("User-Details.csv", index=False)
+    df = initialize_csv()
+    registry_check = input("Are you a registered user? (Y/N): ").strip().upper()
 
-    registryCheck = input("Are you a registered user? (Y/N): ")
-
-    if registryCheck.upper() == "N":
-        while True:
-            print("USER REGISTRATION")
-            user_ic = input("Enter IC Number to register: ")
-            user_pass = input("Enter password to register (Last 4 digits of IC Number): ")
-            if verify_user(user_ic, user_pass):
-                break
-            else:
-                print("Invalid IC Number. Format accordingly \"012345678910\" or \"012345-67-8910\" \n")
-
-        ic_df = pd.DataFrame({
-            "IC Number": [user_ic],
-            "Income": [""],
-            "Tax Relief": [""],
-            "Tax Payable": [""]
-        })
-
-        df = pd.concat([df, ic_df], ignore_index=True)
-        df.to_csv("User-Details.csv", index=False)
-    
-    elif registryCheck.upper() == "Y":
-        while True:
-            print("USER LOGIN")
-            user_ic = input("Enter IC Number: ")
-            user_pass = input("Enter password: ")
-            if verify_user(user_ic, user_pass):
-                
-                user_search = df[(df["IC Number"] == user_ic) & (df["Password"] == user_pass)]
-                if user_search.empty:
-                    print("User record not found. Please register.\n")
-                    print("Exiting program\n")
-                    return
-                else:
-                    print("User record found.")
-                    break
-            else:
-                print("Invalid IC Number. Format accordingly \"012345678910\" or \"012345-67-8910\" \n")
-            
+    if registry_check == "N":
+        register_user(df)
+    elif registry_check == "Y":
+        if not login_user(df):
+            return
+        # continue with rest of your program for logged-in users
     else:
         print("Invalid input. Exiting program.")
 
